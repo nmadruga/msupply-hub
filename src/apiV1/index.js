a43982fcaba63600b599b6a09533818bb4a644bf
andreievg <andrei.evguenov@gmail.com>
2018-03-21 22:35:10 +0545

    Route event end point

--- src/apiV1/index.js
+++ src/apiV1/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import site from './site';
+import event from './event';
 
 export default opts => {
   const api = Router();
 
   api.post('/site/:UUID', site(opts));
+  api.post('/event', event(opts));
 
   return api;
 };

0649f9a5ec8fec9ad165dabda57f887e9bfa51e6
andreievg <andrei.evguenov@gmail.com>
2018-03-21 21:03:26 +0545

    Await insertion

--- src/database/helpers.js
+++ src/database/helpers.js
@@ -3,6 +3,6 @@ export const checkAddNewSite = async (db, UUID, siteInfo, newJWT) => {
   if (foundCount.count === '1') return null;
 
   const insertStatement = 'INSERT INTO "sites" ("UUID", jwt, data) VALUES ($1, $2, $3)';
-  db.none(insertStatement, [UUID, newJWT, siteInfo]);
+  await db.none(insertStatement, [UUID, newJWT, siteInfo]);
   return newJWT;
 };

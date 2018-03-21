50d2fc7fda85d7f7df7b2ec21a287e100f1207ce
andreievg <andrei.evguenov@gmail.com>
2018-03-21 21:04:34 +0545

    site route

---
+++ src/apiV1/site.js
@@ -0,0 +1,35 @@
+import { decodeJWT, encodeJWT } from './helpers';
+import { checkAddNewSite } from '../database';
+
+export default ({ config, db }) => async (req, res, next) => {
+  try {
+    const decodedToken = decodeJWT(req.headers.authorization, config);
+    if (!decodedToken) {
+      return res.status(401).send({
+        authorized: false,
+        message: 'authorization header missing or JWT token is invalid',
+      });
+    }
+    const UUID = req.params.UUID;
+    const newJWT = encodeJWT({
+      type: 'site',
+      UUID,
+    }, config);
+
+    if (!checkAddNewSite(db, UUID, req.body, newJWT)) {
+      return res.status(401).send({
+        authorized: false,
+        message: 'UUID already registered',
+      });
+    }
+
+    return res.send({
+      authorized: true,
+      token: newJWT,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// TODO: add route to documentation

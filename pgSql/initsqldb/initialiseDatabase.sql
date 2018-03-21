4344d3ae35cf567c2649ab1c05866dcf023481bd
andreievg <andrei.evguenov@gmail.com>
2018-03-21 22:33:57 +0545

    Add events table

--- pgSql/initsqldb/initialiseDatabase.sql
+++ pgSql/initsqldb/initialiseDatabase.sql
@@ -3,7 +3,17 @@ DROP TABLE IF EXISTS "version";
 CREATE TABLE "sites" (
    "UUID" varchar,
    "jwt" varchar,
-   "create" TIMESTAMP NOT NULL DEFAULT NOW(),
+   "created" TIMESTAMP NOT NULL DEFAULT NOW(),
    "data" jsonb,
    PRIMARY KEY ("UUID")
+);
+
+CREATE TABLE "events" (
+   id serial,
+   "siteUUID" varchar,
+   "created" TIMESTAMP NOT NULL DEFAULT NOW(),
+   "triggered" TIMESTAMP,
+   "type" varchar,
+   "data" jsonb,
+   PRIMARY KEY (id)
 );

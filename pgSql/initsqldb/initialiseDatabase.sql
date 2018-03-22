560463d48a878875d88bce386dc3eae87124120e
andreievg <andrei.evguenov@gmail.com>
2018-03-22 10:06:17 +0545

    Created not create for timestamp field

--- pgSql/initsqldb/initialiseDatabase.sql
+++ pgSql/initsqldb/initialiseDatabase.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS "version";
 CREATE TABLE "sites" (
    "UUID" varchar,
    "jwt" varchar,
-   "create" TIMESTAMP NOT NULL DEFAULT NOW(),
+   "created" TIMESTAMP NOT NULL DEFAULT NOW(),
    "data" jsonb,
    PRIMARY KEY ("UUID")
 );

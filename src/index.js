08218c7b85bdea0723d28f971a54c9472ac0f200
nmadruga <nicole@sussol.net>
2018-07-04 15:38:39 +1200

    Make suggested changes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { unregister } from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// Because service workers were previously set up, unregister existing
-// users from the service worker. See https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#making-a-progressive-web-app
-unregister();
+registerServiceWorker();
